0f79592561a9dd77e4bddaafca90bc95a7d91cb9
Danny van Duist <dvduist@hotmail.nl>
2019-05-09 13:08:00 +0200

    Made us able to change 'frequency' with 1, 2 and 3 (above "W" on QWERTY keyboard).

--- Project_Exposure/Assets/Scripts/ShootScript.cs
+++ Project_Exposure/Assets/Scripts/ShootScript.cs
@@ -2,28 +2,72 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+enum Frequency
+{
+    LOW = -1,
+    MEDIUM = 0,
+    HIGH = 1,
+}
+
 public class ShootScript : MonoBehaviour
 {
-	[SerializeField]
-	GameObject _bullet;
+    [SerializeField]
+    GameObject _bulletType1;
+    [SerializeField]
+    GameObject _bulletType2;
+    [SerializeField]
+    GameObject _bulletType3;
+
     [SerializeField]
     float _speed = 5000f;
+
+    Dictionary<Frequency, GameObject> _waves = new Dictionary<Frequency, GameObject>();
+
+    Frequency _shootingFrequency = Frequency.MEDIUM;
+
     // Start is called before the first frame update
     void Start()
     {
+        _waves.Add(Frequency.LOW, _bulletType1);
+        _waves.Add(Frequency.MEDIUM, _bulletType2);
+        _waves.Add(Frequency.HIGH, _bulletType3);
     }
 
     // Update is called once per frame
     void Update()
     {
-		Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition + Vector3.forward * 5f);
+        switchWave();
 
-		transform.LookAt(mouseWorldPosition);
-		transform.Rotate(Vector3.right, 90f);
+        Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition + Vector3.forward * 5f);
 
-		if(Input.GetMouseButtonDown(0)){
-			GameObject bullet = Instantiate(_bullet, transform.position, Quaternion.LookRotation(transform.up));
-			bullet.GetComponent<Rigidbody>().AddForce(transform.up * _speed);
-		}
-	}
+        transform.LookAt(mouseWorldPosition);
+        transform.Rotate(Vector3.right, 90f);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject bullet = Instantiate(_waves[_shootingFrequency], transform.position, Quaternion.LookRotation(transform.up));
+            bullet.GetComponent<Rigidbody>().AddForce(transform.up * _speed);
+        }
+    }
+
+    private void switchWave()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            _shootingFrequency = Frequency.LOW;
+            return;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            _shootingFrequency = Frequency.MEDIUM;
+            return;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            _shootingFrequency = Frequency.HIGH;
+            return;
+        }
+    }
 }
