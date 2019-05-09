3608bfe5a0884f9b367ac4ebe88f6d9e9be5e9cb
MY-LITTLE-CHAD\Jesse <dron20d@mail.ru>
2019-05-09 13:09:00 +0200

    T-rash

---
+++ Project_Exposure/Assets/Scripts/ObstacleScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleScript : MonoBehaviour
+{
+    void OnCollisionEnter(Collision other)
+    {
+        Shatter();
+    }
+
+    void Shatter()
+    {
+        //just destroy it for now
+        Destroy(gameObject);
+    }
+}
