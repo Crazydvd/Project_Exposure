dcafbf2531df0fb379cd0f3ee74316243afc0279
MY-LITTLE-CHAD\Jesse <dron20d@mail.ru>
2019-05-10 15:47:14 +0200

    Shattarale objects

---
+++ Project_Exposure/Assets/Scripts/ShatterObject.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShatterObject : MonoBehaviour
+{
+    [SerializeField]
+    float _speed = 10f;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Shatter();
+        }
+    }
+
+    private void Shatter()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            if (transform.GetChild(i).GetComponent<Rigidbody>() == null)            
+                Debug.Log("YOU FORGOT TO ADD KINEMATIC RIGIDBODY TO THE CHILD!!!");
+            
+            Rigidbody childRigid = transform.GetChild(i).GetComponent<Rigidbody>();
+            Transform childTransform = transform.GetChild(i).GetComponent<Transform>();
+            childRigid.isKinematic = false;
+
+            Vector3 direction = (childTransform.position - transform.position).normalized;
+            Vector3 randomizedDirection = new Vector3(direction.x * Random.Range(0.5f, 1.5f), direction.y * Random.Range(0.5f, 1.5f), direction.z * Random.Range(0.5f, 1.5f));
+
+            childRigid.AddForce(randomizedDirection * _speed, ForceMode.Impulse);
+            childRigid.angularVelocity = new Vector3(Random.Range(0f, 10f), Random.Range(0f, 10f), Random.Range(0f, 10f)) * 2;
+        }
+
+        transform.DetachChildren();
+        Destroy(gameObject);
+    }
+
+}
