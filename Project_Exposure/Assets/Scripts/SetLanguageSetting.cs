a1c1c3bb59973bdbdde68ccf40463b0484d84752
Danny van Duist <dvduist@hotmail.nl>
2019-05-20 13:18:00 +0200

    Language is now an enumeration instead of an integer.

--- Project_Exposure/Assets/Scripts/SetLanguageSetting.cs
+++ Project_Exposure/Assets/Scripts/SetLanguageSetting.cs
@@ -8,6 +8,6 @@ public class SetLanguageSetting : MonoBehaviour
 
     public void SetLanguage()
     {
-        LanguageSettings.Language = _language;
+        LanguageSettings.Language = (Language) _language;
     }
 }
