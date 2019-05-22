c6a09ad6abfd3184b9ff2b456fc1855856702a70
Luuk Oude Vrielink <l-oudevrielink@hotmail.com>
2019-05-22 15:18:25 +0200

    Some cool stuff I suppose

---
+++ Project_Exposure/Assets/GameManagerScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManagerScript : MonoBehaviour
+{
+    public void ReloadScene(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    
+    public void QuitGame(){
+        Application.Quit();
+    }
+}
