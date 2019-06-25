69206af6c6fd28aad3ced732e4d954625d27d9c4
MarisaLublewski <riribealub@gmail.com>
2019-06-25 19:45:01 +0200

    2nd level

---
+++ Project_Exposure/Assets/Scripts/OnObjectDestroy.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class OnObjectDestroy : MonoBehaviour
+{
+    enum Mode
+    {
+        ENABLE,
+        DISABLE,
+        TOGGLE,
+    }
+
+    [Header("Objects that are activated")]
+    [SerializeField] GameObject[] _objects;
+
+    [Header("What to do with the objects")]
+    [SerializeField] Mode _mode = Mode.ENABLE;
+
+    void OnDestroy()
+    {
+        foreach (GameObject @object in _objects)
+        {
+            @object.SetActive(true); 
+        }
+    }
+}
