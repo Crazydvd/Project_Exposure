c7995bec9730727ecee0c6ec4eab8fea57399d22
Danny van Duist <dvduist@hotmail.nl>
2019-06-25 19:12:02 +0200

    Put in wave backgrounds, added language support for the video.

---
+++ Project_Exposure/Assets/Scripts/Video/VideoScript.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoScript : MonoBehaviour
+{
+    [SerializeField] VideoPlayer _video;
+
+    [SerializeField] VideoClip _dutch = null;
+    [SerializeField] VideoClip _english = null;
+    [SerializeField] VideoClip _german = null;
+
+    void Start()
+    {
+        if (_video == null)
+        {
+            throw new System.Exception("Forgot to drag a video to this script");
+        }
+
+        setVideo();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        _video?.Play();
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        _video?.Stop();
+    }
+
+    void setVideo()
+    {
+        Language language = LanguageSettings.Language;
+        VideoClip clip = getClip(language);
+
+        while (language > 0 && clip == null)
+        {
+            language--;
+            clip = getClip(language);
+        }
+
+        if (clip == null)
+        {
+            return;
+        }
+
+        _video.clip = clip;
+    }
+
+    VideoClip getClip(Language pLanguage)
+    {
+        switch (pLanguage)
+        {
+            case Language.NL:
+                return _dutch;
+            case Language.EN:
+                return _english;
+            case Language.DE:
+                return _german;
+            default:
+                return _dutch;
+        }
+    }
+}
